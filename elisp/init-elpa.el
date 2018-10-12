8040c2ae8f7415712d1ae401fd38c4a862d615d5
0xMackenzie <0xMackenzie@users.noreply.github.com>
2018-10-12 10:44:40 -0500

    Rework (#1)
    * initial setup of init

---
+++ elisp/init-elpa.el
@@ -0,0 +1,23 @@
+;;; package --- ELPA init
+;;; Commentary:
+;;; Initialize locating package repositories
+;;; Code:
+(require 'package)
+;;; Standard package repositories
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+
+(defun require-package (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (package-install package)
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
+
+(provide 'init-elpa)
+;;; init-elpa.el ends here
---
+++ init.el
@@ -0,0 +1,48 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Root file of configuration.
+;;; Code:
+(package-initialize)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'load-path (expand-file-name "elisp" user-emacs-directory))
+
+(require 'init-elpa)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(setq inhibit-splash-screen t
+      inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(blink-cursor-mode 0)
+(when (boundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+(show-paren-mode 1)
+
+(global-hl-line-mode 1)
+
+(setq visual-line-fringe-indicator '(left-curly-arrow right-curly-arrow))
+(setq-default left-fringe-width nil)
+(setq-default indicate-empty-lines t)
+(setq-default indent-tabs-mode nil)
+
+(setq visible-bell t)
+(setq vc-follow-symlinks t)
+(setq large-file-warning-threshold nil)
+(setq split-width-threshold nil)
+(setq custom-safe-themes t)
+(column-number-mode t)
+(setq tab-width 2)
+(setq tramp-default-method "ssh")
+
+(provide 'init)
+;;; init.el ends here
