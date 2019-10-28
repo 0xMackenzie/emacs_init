4472c3b30054395e53f5ca58ad9f1fe9d025ed42
Mark Mackenzie <markm@alumni.utexas.net>
2019-10-28 07:21:06 -0500

    Setting up support for Elixir Development

---
+++ elisp/init-elixir.el
@@ -0,0 +1,9 @@
+;;; package --- Elixir init
+;;; Commentary:
+;;; Initialize Elixir Mode and configuration
+;;; Code:
+
+(use-package elixir-mode :ensure t)
+
+(provide 'init-elixir)
+;;; init-elixir ends here
